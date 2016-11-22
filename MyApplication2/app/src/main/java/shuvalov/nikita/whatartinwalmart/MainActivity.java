dc4278db0a3c5c50a8c4239bdf3f40f7880e1bb3
NikShuvalov <nshuvalov89@gmail.com>
2016-11-21 23:10:53 -0500

    Replaced download task with VolleyRequest

--- MyApplication2/app/src/main/java/shuvalov/nikita/whatartinwalmart/MainActivity.java
+++ MyApplication2/app/src/main/java/shuvalov/nikita/whatartinwalmart/MainActivity.java
@@ -2,7 +2,6 @@ package shuvalov.nikita.whatartinwalmart;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,17 +10,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.ArrayList;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static final String API_KEY= "6we7gvkbu5sujb78rge8y5dy";
@@ -43,16 +43,14 @@ public class MainActivity extends AppCompatActivity {
         teaButt= (Button)findViewById(R.id.tea_button);
         cerealButt= (Button)findViewById(R.id.cereal_button);
 
-
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
                 NetworkInfo info = connectivityManager.getActiveNetworkInfo();
-
                 String searchParam="";
                 if(info!=null && info.isConnected()){
-                    DownloadTask downloadTask = new DownloadTask();
+                    RequestQueue queue = Volley.newRequestQueue(view.getContext());
                     switch (view.getId()){
                         case R.id.cereal_button:
                             searchParam="cereal";
@@ -64,9 +62,38 @@ public class MainActivity extends AppCompatActivity {
                             searchParam="tea";
                             break;
                     }
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, String.format(REQUEST_URL, searchParam), new com.android.volley.Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            ArrayList<WalmartItem> walmartItems = new ArrayList<>();
+                            try {
+                                JSONObject fullData = new JSONObject(response);
+                                JSONArray itemsArray = fullData.getJSONArray("items");
+                                for (int i = 0; i<itemsArray.length();i++){
+                                    JSONObject individualItem= itemsArray.getJSONObject(i);
+                                    String name = individualItem.getString("name");
+                                    walmartItems.add(new WalmartItem(name));
+                                }
+                                mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
 
-                    String searchURL = String.format(REQUEST_URL,searchParam);
-                    downloadTask.execute(searchURL);
+                                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL,false);
+                                mAdapter = new RecyclerWalmartAdapter(walmartItems);
+                                mRecyclerView.setLayoutManager(linearLayoutManager);
+                                mRecyclerView.setAdapter(mAdapter);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new com.android.volley.Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    queue.add(stringRequest);
+
 
                 }else{
                     Toast.makeText(view.getContext(), "No Internet Connection", Toast.LENGTH_SHORT).show();
@@ -76,45 +103,6 @@ public class MainActivity extends AppCompatActivity {
         chocoButt.setOnClickListener(onClickListener);
         teaButt.setOnClickListener(onClickListener);
         cerealButt.setOnClickListener(onClickListener);
-
     }
-    private class DownloadTask extends AsyncTask<String, Void, String>{
 
-        @Override
-        protected String doInBackground(String... urls) {
-            OkHttpClient okHttpClient = new OkHttpClient();
-            Request request = new Request.Builder().url(urls[0]).build();
-            try {
-                return okHttpClient.newCall(request).execute().body().string();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            ArrayList<WalmartItem> walmartItems = new ArrayList<>();
-            try {
-                JSONObject fullData = new JSONObject(s);
-                JSONArray itemsArray = fullData.getJSONArray("items");
-                for (int i = 0; i<itemsArray.length();i++){
-                    JSONObject individualItem= itemsArray.getJSONObject(i);
-                    String name = individualItem.getString("name");
-                    walmartItems.add(new WalmartItem(name));
-                }
-                mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL,false);
-                mAdapter = new RecyclerWalmartAdapter(walmartItems);
-                mRecyclerView.setLayoutManager(linearLayoutManager);
-                mRecyclerView.setAdapter(mAdapter);
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
 }
